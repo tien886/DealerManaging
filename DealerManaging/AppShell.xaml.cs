43600047e2c210165c4e860d5e1e8f88159f18ee
votie <votie@MINHTIEN>
2025-08-29 14:01:20 +0700

    Add project files.

---
+++ DealerManaging/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DealerManaging
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DealerManaging/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DealerManaging")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DealerManaging.Pages")]
