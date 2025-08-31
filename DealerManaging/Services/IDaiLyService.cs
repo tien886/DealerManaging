e9ad0e7bc5824e307f2b76edd60ac92af4d85f83
TienTheKnightMare <tienvm886@gmail.com>
2025-08-31 09:23:33 +0700

    Initialization of models

---
+++ DealerManaging/Services/IDaiLyService.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DealerManaging.Models;
+namespace DealerManaging.Services
+{
+    public interface IDaiLyService
+    {
+        Task<DaiLy> GetDaiLyById(int id);
+        Task<IEnumerable<DaiLy>> GetAllDaiLy();
+        Task<IEnumerable<DaiLy>> GetDaiLyPage(int offset, int size = 20);
+        Task<int> GetTotalPages(int size = 20);
+        Task AddDaiLy(DaiLy daiLy);
+        Task UpdateDaiLy(DaiLy daiLy);
+        Task DeleteDaiLy(int id);
+        Task<DaiLy> GetDaiLyByTenDaiLy(string tenDaiLy);
+        Task<int> GenerateAvailableId();
+        Task<List<DaiLy>> GetDaiLysByIdsAsync(IEnumerable<int> ids);
+        Task<int> GetTotalDaiLyUpToMonthYear(int month, int year);
+        Task<DateTime?> GetEarliestDaiLyDateAsync();
+    }
+}
---
+++ DealerManaging/Services/ILoaiDaiLyService.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DealerManaging.Services
+//{
+//     public class ILoaiDaiLyService
+//    {
+//        Task<LoaiDaiLy> GetLoaiDaiLyById(int id);
+//        Task<IEnumerable<LoaiDaiLy>> GetAllLoaiDaiLy();
+//        Task AddLoaiDaiLy(LoaiDaiLy loaiDaiLy);
+//        Task UpdateLoaiDaiLy(LoaiDaiLy loaiDaiLy);
+//        Task DeleteLoaiDaiLy(int id);
+//        Task<int> GenerateAvailableId();
+//    }
+//}
