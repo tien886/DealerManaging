939016cb7cf2b478fbc205ec6ca25e1f292a691c
TienTheKnightMare <tienvm886@gmail.com>
2025-08-31 09:23:50 +0700

    fuvk

---
+++ DealerManaging/Configs/DatabaseConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+namespace DealerManaging.Configs;
+
+public class DatabaseConfig
+{
+    private readonly SQLiteAsyncConnection _database;
+    public DatabaseConfig(string db_path)
+    {
+        //_database = new SQLiteAsyncConnection(db_path);
+        //_database.CreateTableAsync<>
+    }
+
+}
+
